76bb3b297e87ed4d0c140bb2aec2ba3cf54881d9
fioredanchoug <fioredanchoug@gmail.com>
2024-07-03 12:08:02 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "153.92.15.25";
-$user = "ojie72";
-$pass = "Ojie7248";
-$db = "tugaskp";
-
-$conn = mysqli_connect($host, $user, $pass, $db) or die("Database MYSQL Tidak ditemukan");
