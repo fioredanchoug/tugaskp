b1f51f83f1172114dc9de72e7ca46803ee3a474a
fioredanchoug <fioredanchoug@gmail.com>
2024-07-12 11:26:05 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "admin";
-$pass = "admin123";
+$user = "ojie72";
+$pass = "ojie7248";
 $db = "tugaskp";
 
 $conn = mysqli_connect($host, $user, $pass, $db) or die("Database MYSQL Tidak ditemukan");
