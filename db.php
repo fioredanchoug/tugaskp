eb7ad5ec358c5106caf299935bc97b0e860206bd
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 18:04:52 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,14 @@
 <?php
 
-session_start();
+$servername = "https://auth-db1367.hstgr.io/";  
+$username = "u644123111_ojie72";     
+$password = "L1nkstart!";     
+$dbname = "u644123111_tugaskp";  
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-
-$servername = "153.92.15.25";
-$username = "u644123111_ojie72";
-$password = "L1nkstart!";
-$dbname = "u644123111_tugaskp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
