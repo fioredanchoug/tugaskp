d928a5f09dc982c795cc4613463884dc466f847a
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 21:48:27 +0700

    Update login.php

--- login.php
+++ login.php
@@ -14,6 +14,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
+
 	if (empty($uname)) {
 		header("Location: index.php?error=User Name is required");
 	    exit();
@@ -23,17 +24,23 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	}
 	else{
 		$sql = "SELECT * FROM users WHERE username='$uname' AND password='$pass'";
-
 		$result = mysqli_query($conn, $sql);
+		
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $uname && $row['password'] === $pass) {
+            if ($row['username'] === $uname && $row['password'] === $pass && $row['usertype'] === "admin") {
             	$_SESSION['username'] = $row['username'];
             	$_SESSION['id'] = $row['id'];
             	header("Location: welcome.php");
 		        exit();
-            }else{
+            }if ($row['username'] === $uname && $row['password'] === $pass && $row['usertype'] === "users") {
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['id'] = $row['id'];
+            	header("Location: welcome.php");
+		        exit();
+            }
+	    else{
 				header("Location: index.php?error=Incorect User name or password");
 		        exit();
 			}
