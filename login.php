1052d583fe91755608b5f80a17517f51cc5d3717
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 21:00:39 +0700

    Update login.php

--- login.php
+++ login.php
@@ -25,6 +25,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 		$sql2 = "SELECT * FROM admin WHERE username='$uname' AND password='$pass'";
 
 		$result1 = mysqli_query($conn, $sql, );
+		$result2 = mysqli_query($conn, $sql2, );
 
 		if (mysqli_num_rows($result1) === 1) {
 			$row = mysqli_fetch_assoc($result1);
