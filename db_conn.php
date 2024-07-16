773b95e508da9996c7b231cb2e0218bf146ec27b
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 20:00:51 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,10 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
+$sname= "https://auth-db1367.hstgr.io/";
+$unmae= "u644123111_ojie7248";
+$password = "Ojie7248";
 
-$db_name = "test";
+$db_name = "u644123111_kp";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
