3cb78731e8424131f4e87be2f3e9d9bb7a305c14
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 20:10:18 +0700

    Update login.php

--- login.php
+++ login.php
@@ -40,6 +40,26 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 			header("Location: index.php?error=Incorect User name or password");
 	        exit();
 		}
+	}else{
+		$sql = "SELECT * FROM admin WHERE username='$uname' AND password='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['id'] = $row['id'];
+            	header("Location: welcome.php");
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorect User name or password");
+	        exit();
+		}
 	}
 	
 }else{
