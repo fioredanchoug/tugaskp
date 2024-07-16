7e768211eec6055e796668a5e20d1b78a49280f6
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 20:22:18 +0700

    Update login.php

--- login.php
+++ login.php
@@ -20,19 +20,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	}else if(empty($pass)){
         header("Location: index.php?error=Password is required");
 	    exit();
-	}else{
-		$sql = "SELECT * FROM admin WHERE username='$uname' AND password='$pass'";
-
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['username'] = $row['username'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: welcome.php");
-		        exit();
-            }
+	}
 	else{
 		$sql = "SELECT * FROM users WHERE username='$uname' AND password='$pass'";
 
