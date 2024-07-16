2ebe593cae4a1aee95f3fc1939d31565425e0e0e
fioredanchoug <fioredanchoug@gmail.com>
2024-07-16 20:55:53 +0700

    Update login.php

--- login.php
+++ login.php
@@ -22,8 +22,9 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	    exit();
 	}else{
 		$sql = "SELECT * FROM users WHERE username='$uname' AND password='$pass'";
+		$sql2 = "SELECT * FROM users WHERE username='$uname' AND password='$pass'";
 
-		$result = mysqli_query($conn, $sql);
+		$result = mysqli_query($conn, $sql, $sql2);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
